8e54a845cc29410de1b03c65c1fdbd042298ee43
h0341a <qianaiwuyu@gmail.com>
2020-01-28 17:17:01 +0800

    The side nav menu is finished

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,72 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="fillcontain">
+    <el-container class="fillcontain">
+      <el-header></el-header>
+      <el-container>
+        <el-aside>
+          <el-row class="tac">
+            <el-col>
+              <el-menu default-active="1" :default-openeds="['1']" router class="el-menu-vertical-demo">
+                <el-submenu index="1">
+                  <template slot="title">
+                    <i class="el-icon-menu"></i>
+                    <span>分组列表</span>
+                  </template>
+                  <el-menu-item-group>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                  </el-menu-item-group>
+                </el-submenu>
+              </el-menu>
+            </el-col>
+          </el-row>
+        </el-aside>
+        <el-main>main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scope>
+html,
+body,
+#app,
+.el-container {
+  padding: 0px;
+  margin: 0px;
+  height: 100%;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
   text-align: center;
-  color: #2c3e50;
+  line-height: 60px;
 }
 
-#nav {
-  padding: 30px;
+.el-aside {
+  width: 200px !important;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
 }
 </style>
